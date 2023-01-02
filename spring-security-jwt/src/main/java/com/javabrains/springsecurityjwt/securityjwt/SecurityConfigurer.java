d204492bd0edd73031c6eb2b1253d42003b78e50
rittima <81369850+rittima@users.noreply.github.com>
2023-01-02 14:28:25 +0530

    Add files via upload

---
+++ spring-security-jwt/src/main/java/com/javabrains/springsecurityjwt/securityjwt/SecurityConfigurer.java
@@ -0,0 +1,61 @@
+package com.javabrains.springsecurityjwt.securityjwt;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.javabrains.springsecurityjwt.filter.JwtFiltersPacket;
+import com.javabrains.springsecurityjwt.securityjwt.service.MyUserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter
+{
+	@Autowired
+	private MyUserDetailsService myUserDetailsService;
+	
+	@Autowired
+	private JwtFiltersPacket jwtRequestFilter;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+		
+		auth.userDetailsService(myUserDetailsService);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests().antMatchers("/authenticate").permitAll().
+			anyRequest().authenticated()
+			.and().sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
---
+++ springsecurity-facebook/src/test/java/com/javabrains/springsecurityfacebook/SpringsecurityFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabrains.springsecurityfacebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
