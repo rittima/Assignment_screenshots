13156e5ada582da1072d57a072804c9e1fce62d0
rittima <81369850+rittima@users.noreply.github.com>
2023-01-02 14:26:32 +0530

    Add files via upload

---
+++ Spring-Security-Assignments-1/src/main/java/com/example/springsecurity/SpringSecurityAssignments1Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAssignments1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAssignments1Application.class, args);
+	}
+
+}
